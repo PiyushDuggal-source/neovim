27246ab32948b65211e5ef1029a21101c5124dff
Piyush Duggal <duggalpiyush0@gmail.com>
2023-11-05 18:30:00 +0530

    removed unused plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,7 +91,7 @@ return packer.startup(function(use)
 
   -- LSP
   use({ "neovim/nvim-lspconfig" })           -- enable LSP
-  use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
+  -- use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
   use { "williamboman/mason.nvim",
     config = function()
       require "user.lsp.mason"
