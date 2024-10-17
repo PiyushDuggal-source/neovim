64efbbff4cb144d6c159379b7ecbf79e4d4bdb39
Piyush Duggal <duggalpiyush0@gmail.com>
2024-10-16 23:32:53 -0400

    update codeium for mac

--- lua/user/codeium.lua
+++ lua/user/codeium.lua
@@ -1,2 +1,12 @@
+--
+
+if vim.fn.has "mac" == 1 then
+vim.cmd[[imap <silent><script><expr> <C-i> codeium#Accept()]]
+else
+
 vim.cmd[[imap <silent><script><expr> <M-i> codeium#Accept()]]
+end
+
+-- vim.api.nvim_set_keymap('n', '<D-i>', ':call codeium#Accept()<CR>', { noremap = true, silent = true })
+-- -- vim.api.nvim_set_keymap('i', '<D-i>', '<C-o>:call codeium#Accept()<CR>', { noremap = true, silent = true })
 vim.g.codeium_no_map_tab = true
